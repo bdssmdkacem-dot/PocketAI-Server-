4344cdfa1c432ac4a6d18325c9e3f54d26ac4fee
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-22 15:53:37 +0100

    fix: keep initial Flutter APK build independent of native library

--- android/pocketai_template/app/src/main/kotlin/com/pocketai/server/MainActivity.kt
+++ android/pocketai_template/app/src/main/kotlin/com/pocketai/server/MainActivity.kt
@@ -2,10 +2,4 @@ package com.pocketai.server
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-    companion object {
-        init {
-            System.loadLibrary("pocket_ai")
-        }
-    }
-}
+class MainActivity : FlutterActivity()
