5863d7f37cd926fad58987341840cd657b288be1
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-24 10:59:17 +0000

    feat: expose Android device capability diagnostics

--- android/pocketai_template/app/src/main/kotlin/com/pocketai/server/MainActivity.kt
+++ android/pocketai_template/app/src/main/kotlin/com/pocketai/server/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.pocketai.server
 
+import android.app.ActivityManager
+import android.os.Build
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -20,10 +22,28 @@ class MainActivity : FlutterActivity() {
                             "status" to nativeAi.status(),
                             "version" to nativeAi.version(),
                             "runtime" to nativeAi.runtimeCheck(),
+                            "device" to deviceCapabilities(),
                         ),
                     )
                     else -> result.notImplemented()
                 }
             }
+    }
+
+    private fun deviceCapabilities(): Map<String, Any> {
+        val memoryInfo = ActivityManager.MemoryInfo()
+        val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        activityManager.getMemoryInfo(memoryInfo)
+
+        return mapOf(
+            "manufacturer" to Build.MANUFACTURER,
+            "model" to Build.MODEL,
+            "androidApi" to Build.VERSION.SDK_INT,
+            "supportedAbis" to Build.SUPPORTED_ABIS.toList(),
+            "cpuCores" to Runtime.getRuntime().availableProcessors(),
+            "totalRamBytes" to memoryInfo.totalMem,
+            "availableRamBytes" to memoryInfo.availMem,
+            "lowMemory" to memoryInfo.lowMemory,
+        )
     }
 }
